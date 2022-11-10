511474e56e4f7752cc176ee6667086b47c38869d
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-11-10 17:27:14 -0300

    implementando logica da validação do cadastro

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -8,6 +8,76 @@ const estadoInput = document.querySelector("#estado");
 
 const formInputs = document.querySelectorAll("[data-input]");
 
+//classe de validação
+class Validar {
+    constructor() {
+        this.validations = [
+            'data-min-length',
+        ]
+    }
+
+    //Validação dos campos
+    validate(form) {
+        let inputs = form.getElementsByTagName('input');
+
+        //transformando html colection em array
+        let inputsArray = [...inputs];
+
+        //loop nos inputs e validação
+        inputsArray.forEach(function(input) {
+
+            //loop em todas as validações
+            for(let i = 0; this.validations.length > i; i++) {
+                //verificação se existe no input
+                if (input.getAttribute(this.validations[i]) != null ) {
+
+                    //transformando string no nome do method
+                    //transformar data-min-length=> minlength
+                    let method = this.validations[i].replace('data-', '').replace('-', '');
+
+                    //valor do input
+                    let value = input.getAttribute(this.validations[i]);
+
+                    //invocando o method
+                    this[method](input, value);
+
+                }
+            }
+
+        }, this);
+
+    }
+
+    //metodo para min de caracteres (apos ser transformado em metodo)
+    minlength(input, minValue) {
+
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo deve conter pelo menos ${minValue} caracteres`;
+
+        if (inputLength < minValue) {
+            console.log(errorMessage);
+        }
+
+    }
+}
+
+//formulario
+let form = document.getElementById("registro-form");
+let submit = document.getElementById("btn-submit");
+
+//objeto de validação
+let validador = new Validar();
+
+
+//evento para validações
+submit.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    validador.validate(form);
+})
+
+
 
 // Evento pegar endereço
 cepInput.addEventListener("keyup", (e) => {
